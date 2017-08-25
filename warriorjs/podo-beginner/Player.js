0383cd2d405fd284c993d1e5c73aec2ad506da1a
Tommaso Padovan <tommaso.pado@gmail.com>
2017-08-25 17:42:39 +0200

    level 6 solved

---
+++ warriorjs/podo-beginner/Player.js
@@ -0,0 +1,66 @@
+const MAX_HEALTH = 20;
+const GOOD_HEALTH = 15;
+const CRITIC_HEALTH = 7;
+	
+var health=20;
+var underAttack = false;
+
+var exploredBackwards = false;
+var direction = 'backward';
+
+const State = {
+	noOp: 0,
+	walk: 1,
+	rest: 2,
+	attack: 3,
+	rush: 4,
+	retreat: 5
+}
+
+function oppositeDirection(direction) {
+	if (direction == 'backward') return 'forward';
+	else return 'backward';
+}
+
+class Player {
+	
+
+	playTurn(warrior) {
+		if (health > warrior.health()) underAttack = true;
+		else underAttack = false;
+		health = warrior.health()
+
+		if (!warrior.feel(direction).isEmpty()) {
+			if (warrior.feel(direction).isCaptive()){
+				warrior.rescue(direction);
+			}
+
+			else {
+				if (warrior.feel(direction).isWall())
+					direction = 'forward'
+				else
+					warrior.attack(direction)
+			}
+		} else {
+
+			if (warrior.health()>GOOD_HEALTH) {
+				warrior.walk(direction);
+			} else {
+				if (underAttack) {
+					if (warrior.health() <= CRITIC_HEALTH) 
+						warrior.walk(oppositeDirection(direction));			//run awaaaaay bwoy
+					else 
+						warrior.walk(direction);
+				}
+				else
+					warrior.rest();
+			}
+		}
+	}
+
+
+
+}
+
+
+
