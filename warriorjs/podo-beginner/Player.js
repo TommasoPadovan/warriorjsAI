786750033db3eb41cafdd2efafa58a6488a8e517
Tommaso Padovan <tommaso.pado@gmail.com>
2017-09-20 15:41:25 +0200

    addings

--- warriorjs/podo-beginner/Player.js
+++ warriorjs/podo-beginner/Player.js
@@ -8,32 +8,101 @@ function oppositeDirection(direction) {
 }
 
 var i = 0;
+const MAX_HEALTH = 20;
 
 class Player {
 
+    constructor() {
+        this.healthLastTurn = MAX_HEALTH;
+        this.healthThisTurn = MAX_HEALTH;
+
+        this.facing = "right";
+
+        this.actualPoints = 0;
+        this.potentialPoint = 0;
+
+        this.actionLastTurn = Actions.noOp;
+        this.actionThisTurn = Actions.noOp;
+    }
 
     playTurn(warrior) {
-        switch(i) {
-            case 0:
-            case 1:
-            case 2:
-                warrior.walk();
+        console.log(warrior.look('backward'));
+        console.log("####################################");
+
+        this.updateStatus(warrior);
+
+        //this.deltaScoreForSuccessors()
+    };
+
+
+    updateStatus(warrior) {
+        this.healthLastTurn = this.healthThisTurn;
+        this.healthThisTurn = warrior.health();
+
+        this.actionLastTurn = this.actionThisTurn;
+    }
+
+    updatePoints(warrior) {
+        //penalty for each turn spent
+        this.actualPoints --;
+    }
+
+    getSuccessors(warrior) {
+        //base actions
+        const aux = [Actions.noOp,
+                Actions.rest];
+        //movement
+        switch (this.facing) {
+            case "right":
+                if (warrior.feel().isEmpty())
+                    aux.push(Actions.walkLeft);
+                if (warrior.feel('backward').isEmpty())
+                    aux.push(Actions.walkRight);
                 break;
-            case 3:
-                warrior.attack();
-                break;
-            case 4:
-                warrior.pivot();
-                break;
-            case 5:
-                warrior.attack('backward');
+            case "left":
+                if (warrior.feel().isEmpty())
+                    aux.push(Actions.walkRight);
+                if (warrior.feel('backward').isEmpty())
+                    aux.push(Actions.walkLeft);
                 break;
         }
 
-        i++;
+        //attack melee
+        if (warrior.feel().isEnemy())
+            aux.push(Actions.attackMelee);
+        if (warrior.feel('backward').isEnemy())
+            aux.push(Actions.attackMeleeBackward);
+
+        //attack ranged
+        switch (this.facing) {
+            case "right":
+                if (warrior.look().find(space => space.isEnemy()))
+                    aux.push(Actions.attackRangedLeft);
+                if (warrior.look().find(space => space.isEnemy()))
+                    aux.push(Actions.attackRangedRight);
+                break;
+            case "left":
+                if (warrior.look().find(space => space.isEnemy()))
+                    aux.push(Actions.attackRangedRight);
+                if (warrior.look().find(space => space.isEnemy()))
+                    aux.push(Actions.attackRangedLeft);
+                break;
+        }
+
+        //rescue
+        if (warrior.feel().isCaptive())
+            aux.push(Actions.rescue);
+        if (warrior.feel('backward').isCaptive())
+            aux.push(Actions.rescueBackward);
 
 
-    };
+        return aux;
+    }
+
+    seeEnemy(warrior, leftOrRight) {
+
+    }
+
 
 
 
@@ -53,9 +122,33 @@ const Actions = {
     walkLeft: 2,
     walkRight: 3,
     attackMelee: 4,
-    attackMeleeBackwards: 5,
+    attackMeleeBackward: 5,
     attackRangedLeft: 6,
     attackRangedRight: 7,
-    rescue: 8
+    rescue: 8,
+    rescueBackward: 9,
+    pivot: 10
+};
 
+const Enemies = {
+    sludge: {
+        health: 12,
+        dmg:    3,
+        range:  0
+    },
+    thickSludge: {
+        health: 24,
+        dmg:    3,
+        range:  0
+    },
+    archer: {
+        health: 7,
+        dmg:    3,
+        range:  2
+    },
+    wizard: {
+        health: 1,
+        dmg:    11,
+        range:  1
+    }
 };
